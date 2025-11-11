e03890ec9afd9d2ad5d53d6b93f11c03c070160a
Evans Barngetuny <evanskibitok006@gmail.com>
2025-11-11 13:28:51 +0300

    fix: Update CI workflow for MySQL empty password

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,14 @@
 
 namespace Tests\Feature;
 
-use PhpParser\Node\Expr\Cast\Void_;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
-     *
-     * @return void
      */
-    public function test_example(): Void
+    public function test_example(): void
     {
         $response = $this->get('/login');
 
